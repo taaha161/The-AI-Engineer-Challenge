7313b939f6a24f76d178ff6c7ac18ccd1fdfdf15
Taaha <pakitaaha@gmail.com>
2025-06-13 17:13:59 -0500

    update url

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -142,7 +142,7 @@ function Chat() {
     setIsLoading(true);
 
     try {
-      const apiUrl = 'http://localhost:8000';
+      const apiUrl = 'https://aicode-n2nw6eu2f-taaha161s-projects.vercel.app';
       console.log('Sending request to:', apiUrl);
       
       const response = await fetch(`${apiUrl}/api/chat`, {
